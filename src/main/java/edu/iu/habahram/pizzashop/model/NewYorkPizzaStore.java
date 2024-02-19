55f303f55490796d263307d6b2e3ceb188e320c0
jiahaohe <jiahhe@iu.edu>
2024-02-19 09:59:25 -0500

    final

--- src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaStore.java
+++ src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaStore.java
@@ -1,6 +1,6 @@
 package edu.iu.habahram.pizzashop.model;
 
-public class NewYorkPizzaStore {
+public class NewYorkPizzaStore extends PizzaStore {
     protected Pizza createPizza(String item) {
         PizzaFactory pizzaFactory = (PizzaFactory) new NewYorkPizzaFactory();
         return pizzaFactory.createPizza(item);
